5640d13f4281b2647cf013664a8d362bf16398e2
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-02-10 02:38:03 -0500

    Create 2974. Minimum Number Game

---
+++ Solutions/2974. Minimum Number Game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+      //sort the numbers in the list to put them in ascending order
+        nums.sort()
+      //initializing the loop variable
+        i=0
+        while(i<len(nums)):
+          //the first number in the list is swapped with the second number in the list and the process continues till the loop ends which gives us the resulting solution
+            nums[i], nums[i+1]= nums[i+1], nums[i]
+          // the iteration is set to 2 instead of 1
+            i+=2
+          // return the resulting nums array
+        return nums
