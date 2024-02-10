0ffd6b7576591d54b7523693e3948b9afec7d582
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-02-10 02:40:03 -0500

    Update 2974. Minimum Number Game

--- Solutions/2974. Minimum Number Game.py
+++ Solutions/2974. Minimum Number Game.py
@@ -7,7 +7,7 @@ class Solution:
         while(i<len(nums)):
           //the first number in the list is swapped with the second number in the list and the process continues till the loop ends which gives us the resulting solution
             nums[i], nums[i+1]= nums[i+1], nums[i]
-          // the iteration is set to 2 instead of 1
+          // the iteration is set to 2 instead of 1 so that the number of iterations will be half of the given length
             i+=2
           // return the resulting nums array
         return nums
